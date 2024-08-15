daeb50efe6603648d88a8903a9b118cc883c5404
Emanuela Buganik <emanuela.buganik@rt-rk.com>
2024-08-15 14:02:30 +0200

    Added changes to readtest.cpp for successfull serial communication and JSON parsing

--- rpi/readtest.cpp
+++ rpi/readtest.cpp
@@ -3,9 +3,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
-#define START_MARKER 'R'
-#define END_MARKER '\n'
+#include <iostream>
+#include <string.h>
+#include "./ext_lib/json.hpp"
+
+#define START_MARKER '{'
+#define END_MARKER '}'
 #define BUFFER_SIZE 200
+
+
+using namespace std;
+using json = nlohmann::json;
+using namespace nlohmann::literals;
 
 int configure_fd(const char *portname) {
     int fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
@@ -25,16 +34,14 @@ int configure_fd(const char *portname) {
 	SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits cleared, so 1 Stop bit */
 	SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
 	SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
-
 	SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
 	SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,ignore Modem Control lines       */
-
 	SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
 	SerialPortSettings.c_lflag &= ~(ECHO | ECHONL | ICANON|  IEXTEN | ISIG);  /* Non Cannonical mode                            */
 
 	SerialPortSettings.c_oflag &= ~OPOST; /*No Output Processing*/
     
-    SerialPortSettings.c_cc[VMIN]  = 50;
+    SerialPortSettings.c_cc[VMIN]  = 10;
     SerialPortSettings.c_cc[VTIME] = 0;
 
 	if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0){ /* Set the attributes to the termios structure*/
@@ -62,14 +69,25 @@ void read_json_from_serial(int fd) {
 
             if (json_started) {
                 buffer_pos += bytes_read;
-               // printf("%s\n", buffer);
+                // std::cout << buffer << std::endl;
             }
 
             if (buffer[buffer_pos - 1] == END_MARKER) {
-                buffer[buffer_pos] = '\0'; // Terminate string for later
-                printf("Received JSON: %s\n", buffer);
+                buffer[buffer_pos] = '\0'; 
+                try {
+                    std::cout << buffer << std::endl;
+                    std::string jsonString(buffer);
+                    json jsonParsed = json::parse(jsonString);
+                    std::cout << "Received JSON: " << jsonParsed.dump() << std::endl; 
+                    std::cout << jsonParsed["method"] << std::endl;
+                } catch (json::parse_error& e) {
+                    std::cerr << "Parse error: " << e.what() << std::endl;
+                }
+
                 json_started = 0;
                 buffer_pos = 0;
+                // Initialize integer array with zeros
+                memset((void *)buffer,'0',sizeof(buffer));
             }
         }
     }
