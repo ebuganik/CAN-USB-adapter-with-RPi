df7b8a7bdce5bfad5b327f8d410ce7d9cfa81d60
Emanuela Buganik <emanuela.buganik@rt-rk.com>
2024-08-15 09:03:53 +0200

    Added changes to terminal.py script

--- windows/win.py
+++ windows/win.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import serial
 import json
-
-ser = serial.Serial("COM11", 115200,
+import time
+ser = serial.Serial("COM13", 115200,
     parity=serial.PARITY_NONE,
     bytesize=serial.EIGHTBITS,
     stopbits=serial.STOPBITS_ONE,
@@ -10,17 +10,9 @@ ser = serial.Serial("COM11", 115200,
     )
 
 # Creating a python object
-# Cannot be parsed the right way
-# data = {{"method","read"},{"bitrate",250000},
-#         {"id", "0x123"},{"dlc", 4},
-#         {"data","[0x11,0x22,0x33,0x44]"}
-#         }
-# Unable to do this
-# data_raw = R"({"method":"read","bitrate":250000,"id":"0x123","dlc":4,"data":"{0x11,0x22,0x33,0x44}"})" 
-# 1 - Able to do this
-# data = {"method":"read","bitrate":250000,"id":"0x123","dlc":4,"data":"[0x11,0x22,0x33,0x44]"}
 # 2 - Able to do this and I probably will
-data = 'R"({"method":"read","bitrate":250000,"id":"0x123","dlc":4,"data":"[0x11,0x22,0x33,0x44]"})"'
+json_out = {"method":"read","bitrate":250000,"id":"0x123","dlc":4,"data":"[0x11,0x22,0x33,0x44]"}
+# data = 'R"({"method":"read","bitrate":250000,"id":"0x123","dlc":4,"data":"[0x11,0x22,0x33,0x44]"})"'
 # probati i sa ovim
 #data +=str('\n')
 # 3 - Able to to this but cannot parse 
@@ -28,9 +20,15 @@ data = 'R"({"method":"read","bitrate":250000,"id":"0x123","dlc":4,"data":"[0x11,
 # data = json.dumps(data)
 
 # print JSON string
-print(data)
+# print(data)
 # Send over serial
 while True:
+    json_output = json.dumps(json_out)
+    data = f'R"({json_output})"'
+    data += '\n'
+    print(data.encode('utf-8'))
+    time.sleep(3)
     val=ser.write(data.encode('utf-8'))
-    print(f"Bytes written: {val}")
-
+    print(f"bytes written: {val}")
+    #print(ser.readline())
+    # Ovdje dodati provjeru da prilikom primanja stringa provjeri čime počinje i završava
