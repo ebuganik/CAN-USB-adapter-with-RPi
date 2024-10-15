1d6f50be0478bba44670fd358e18dea792bbb2a5
Emanuela Buganik <emanuela.buganik@rt-rk.com>
2024-10-15 14:29:37 +0200

    Added new addition.py file for extra info used in terminal.py and main.py. main.py minimized

--- windows/terminal.py
+++ windows/terminal.py
@@ -1,4 +1,5 @@
 import display as dp
+import addition as add
 import json
 import time
 
@@ -7,61 +8,6 @@ import time
 write_dict = {}
 read_dict = {}
 read_dict_cycle = {}
-
-# Mapping supported bitrates for testing with PEAKCAN-USB (CAN SJA-1000 mode)
-
-PCAN_BITRATES = {
-    5000: "5 kbps",
-    10000: "10 kbps",
-    20000: "20 kbps",
-    33333: "33.333 kbps",
-    40000: "40 kbps",
-    47619: "47.619 kbps",
-    50000: "50 kbps",
-    83333: "83.333 kbps",
-    95238: "95.238 kbps",
-    100000: "100 kbps",
-    125000: "125 kbps",
-    200000: "200 kbps",
-    250000: "250 kbps",
-    500000: "500 kbps",
-    800000: "800 kbps",
-    1000000: "1 Mbps",
-}
-
-# Mapping supported baudrates for serial communication
-SERIAL_BAUDRATE = {
-     50   :'B50',
-     75   :'B75',
-     110  :'B110',
-     134  :'B134',
-     150  :'B150',
-     200  :'B200',
-     300  :'B300',
-     600  :'B600',
-     1200 :'B1200',
-     1800 :'B1800',
-     2400 :'B2400',
-     4800 :'4800',
-     9600 : '9600',
-     19200 : '19200',
-     38400 : '38400',
-     57600 : '57600',
-     115200 : '115200',
-     230400 : '230400',
-     460800 : '460800',
-     500000 : '500000',
-     576000 : '576000',
-     921600 : '921600',
-     1000000 : '1000000',
-     1152000 : '1152000',
-     1500000 : '1500000',
-     2000000 : '2000000',
-     2500000 : '2500000',
-     3000000 : '3000000',
-     3500000 : '3500000',
-     4000000 : '4000000'
-}
 
 # Function to open user manual.txt file 
 def show_manual():
@@ -145,14 +91,14 @@ def read_input():
                         return json_send
             # Print PCAN-USB supported bitrates to choose
             pcan_bitrates_str = "\n ".join(
-                [f"{rate} bps ({PCAN_BITRATES[rate]})" for rate in PCAN_BITRATES]
+                [f"{rate} bps ({add.PCAN_BITRATES[rate]})" for rate in add.PCAN_BITRATES]
             )
             print(f"Supported bitrates:\n {pcan_bitrates_str}")
 
             while True:
                 try:
                     bitrate = int(input("Enter desired bitrate: ").strip())
-                    if bitrate not in PCAN_BITRATES:
+                    if bitrate not in add.PCAN_BITRATES:
                         raise ValueError(
                             f"Unsupported bitrate. Choose one of the supported bitrates."
                         )
@@ -240,14 +186,14 @@ def read_input():
 
             # Print PCAN-USB supported bitrates to choose
             pcan_bitrates_str = "\n ".join(
-                [f"{rate} bps ({PCAN_BITRATES[rate]})" for rate in PCAN_BITRATES]
+                [f"{rate} bps ({add.PCAN_BITRATES[rate]})" for rate in add.PCAN_BITRATES]
             )
             print(f"Supported bitrates:\n {pcan_bitrates_str}")
 
             while True:
                 try:
                     bitrate = int(input("Enter desired bitrate: ").strip())
-                    if bitrate not in PCAN_BITRATES:
+                    if bitrate not in add.PCAN_BITRATES:
                         raise ValueError(
                             f"Unsupported bitrate. Choose one of the supported bitrates."
                         )
